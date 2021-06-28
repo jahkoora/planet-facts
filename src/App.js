87719be1251fb3b8d8c77abcf0015f43110f3554
jahkoora <68049871+jahkoora@users.noreply.github.com>
2021-06-28 12:40:13 +0800

    fixed nav height issue on mobile

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Planet from "./components/Planet";
 import data from "./data.json";
-import './App.css'
+import "./App.css";
 
 export default function App() {
-   const [activePlanet, setActivePlanet] = useState(data[0]);
-   const [menuState, setMenuState] = useState(null)
+  const [activePlanet, setActivePlanet] = useState(data[0]);
+  const [menuState, setMenuState] = useState(null);
 
-   function handleActivePlanet(planet, index) {
-      return setActivePlanet(data[index]);
-   }
+  function handleActivePlanet(planet, index) {
+    return setActivePlanet(data[index]);
+  }
 
-   return (
-      <>
-         <Header
-            planets={data}
-            menuState={menuState}
-            setMenuState={setMenuState}
-            handleActivePlanet={handleActivePlanet}
-            activePlanet={activePlanet}
-         />
-         <Planet activePlanet={activePlanet} />
-      </>
-   );
+  return (
+    <>
+      <Header
+        planets={data}
+        menuState={menuState}
+        setMenuState={setMenuState}
+        handleActivePlanet={handleActivePlanet}
+        activePlanet={activePlanet}
+      />
+      <Planet activePlanet={activePlanet} />
+    </>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,81 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useRef } from "react";
+import useWindowDimensions from "../helpers/useWindowDimensions";
 
+export default function Header({
+  planets,
+  handleActivePlanet,
+  menuState,
+  setMenuState,
+}) {
+  const [selectedPlanet, setSelectedPlanet] = useState("mercury"); // eslint-disable-line
+  const [planetIsActive, setPlanetIsActive] = useState("mercury");
+  const navlist = useRef("");
+  const hamburgerIcon = useRef("");
+  const { height, width } = useWindowDimensions();
 
-export default function Header({ planets, handleActivePlanet, menuState, setMenuState }) {
-   const [selectedPlanet, setSelectedPlanet] = useState("mercury"); // eslint-disable-line
-   const [planetIsActive, setPlanetIsActive] = useState("mercury")
-   const navlist = useRef("")
-   const hamburgerIcon = useRef("")
+  function handleSelectedPlanet(planet, index) {
+    handleActivePlanet(planet, index);
+    setSelectedPlanet(planet.name);
+    setPlanetIsActive(planet.name.toLowerCase());
+    setMenuState(!menuState);
+  }
 
-   function handleSelectedPlanet(planet, index) {
-      handleActivePlanet(planet, index);
-      setSelectedPlanet(planet.name);
-      setPlanetIsActive(planet.name.toLowerCase())
-      setMenuState(!menuState)
-   }
-   
+  if (menuState === true) {
+    navlist.current.style.left = "0";
+    hamburgerIcon.current.style.fill = "rgba(255,255,255,0.2)";
+    document.querySelector("body").style.overflowY = "hidden";
+  } else if (menuState === false) {
+    navlist.current.style.left = "100vw";
+    hamburgerIcon.current.style.fill = "rgba(255,255,255)";
+    document.querySelector("body").style.overflowY = "visible";
+  }
 
-   if(menuState === true){
-      navlist.current.style.left = "0"
-      hamburgerIcon.current.style.fill = "rgba(255,255,255,0.2)"
-      document.querySelector('body').style.overflowY = "hidden"
-   } else if (menuState === false ) {
-      navlist.current.style.left = "100vw"
-      hamburgerIcon.current.style.fill = "rgba(255,255,255)"
-      document.querySelector('body').style.overflowY = "visible"
-   }
+  const isMobile = width < 640;
 
-   return (
-      <header>
-         <nav>
-            <span className="logo">THE PLANETS</span>
-            <ul className="nav-list" ref={navlist}>
-               {planets.map((planet, index) => {
-                  return (
-                     <li className={`
-                     nav-link ${planet.name.toLowerCase()} ${planetIsActive === planet.name.toLowerCase() ? "active" : ""}`}
-                     key={index}>
-                        <a
-                           href="#"
-                           onClick={() => handleSelectedPlanet(planet, index)}
-                        >
-                           {planet.name}
-                        <span  className="chevron"></span>
-                        </a>
-                     </li>
-                  );
-               })}
-            </ul>
-            <button className={`hamburger mobile ${menuState ? 'menu-close' : ''}`} onClick={() => setMenuState(!menuState)}>
-               {/* <img src="images/icon-hamburger.svg" alt="mobile menu button"/> */}
-               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="17"><g ref={hamburgerIcon} fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z"/></g></svg>
-            </button>
-         </nav>
-      </header>
-   );
+  return (
+    <header>
+      <nav>
+        <span className="logo">THE PLANETS</span>
+        <ul
+          className="nav-list"
+          ref={navlist}
+          style={{ height: isMobile ? height - 140 : "" }}>
+          {planets.map((planet, index) => {
+            return (
+              <li
+                className={`
+                    nav-link ${planet.name.toLowerCase()} ${
+                  planetIsActive === planet.name.toLowerCase()
+                    ? "active"
+                    : ""
+                }`}
+                key={index}>
+                <a
+                  href="#"
+                  onClick={() => handleSelectedPlanet(planet, index)}>
+                  {planet.name}
+                  <span className="chevron"></span>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          className={`hamburger mobile ${
+            menuState ? "menu-close" : ""
+          }`}
+          onClick={() => setMenuState(!menuState)}>
+          {/* <img src="images/icon-hamburger.svg" alt="mobile menu button"/> */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="17">
+            <g ref={hamburgerIcon} fill="#FFF" fill-rule="evenodd">
+              <path d="M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z" />
+            </g>
+          </svg>
+        </button>
+      </nav>
+    </header>
+  );
 }
